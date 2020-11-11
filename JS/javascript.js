3c6c35dbfab3e6854f9fad0b99b22e113e8e64f0
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2020-11-10 18:21:10 -0700

    Day 1

---
+++ JS/javascript.js
@@ -0,0 +1,36 @@
+//H1
+var h1 = document.createElement('H1');
+var barik = document.createTextNode("Barik");
+h1.appendChild(barik);
+document.body.appendChild(h1);
+
+//Images
+var dad = document.createElement('IMG');
+dad.src = "./Images/dad.jfif";
+document.body.appendChild(dad);
+
+var flex = document.createElement('IMG');
+flex.src = "./Images/flex.jfif";
+document.body.appendChild(flex);
+
+//Paragraphs
+var p = document.createElement('P');
+    var sent1 = document.createTextNode("Sentence 1");
+    p.appendChild(sent1);
+    document.body.appendChild(p);
+
+    var sent1 = document.createTextNode("Sentence 2");
+    p.appendChild(sent1);
+    document.body.appendChild(p);
+
+    var sent1 = document.createTextNode("Sentence 3");
+    p.appendChild(sent1);
+    document.body.appendChild(p);
+
+    var sent1 = document.createTextNode("Sentence 4");
+    p.appendChild(sent1);
+    document.body.appendChild(p);
+
+    var sent1 = document.createTextNode("Sentence 5");
+    p.appendChild(sent1);
+    document.body.appendChild(p);
